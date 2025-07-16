34e8988283abec2395b4f858a0b44ce2530824ba
Léopold Mebazaa <lemeb@users.noreply.github.com>
2025-07-16 12:30:27 +0200

    style: end of lines etc

--- {{cookiecutter.project_slug}}/tests/test_simple.py
+++ {{cookiecutter.project_slug}}/tests/test_simple.py
@@ -4,5 +4,6 @@ from {{cookiecutter.project_slug}} import {{cookiecutter.project_slug}}
 def test_assert_true() -> None:
     assert True
 
+
 def test_main() -> None:
     assert {{cookiecutter.project_slug}}.main() is True
