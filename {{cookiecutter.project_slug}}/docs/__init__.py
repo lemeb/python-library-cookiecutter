5fddebec5c5938202a654b27be405eecba72813b
Léopold Mebazaa <lemeb@users.noreply.github.com>
2025-07-16 11:31:22 +0200

    docs: docs in the template

---
+++ {{cookiecutter.project_slug}}/docs/__init__.py
@@ -0,0 +1,7 @@
+"""Documentation.
+
+Since this directory might contain code (e.g., examples),
+we treat it as a Python module.
+
+Copyright (c) {% now 'utc', '%Y' %} {{cookiecutter.full_name}}.
+"""
---
+++ {{cookiecutter.project_slug}}/docs/api/main.md
@@ -0,0 +1,5 @@
+# API documentation example
+
+This is an example of API documentation:
+
+::: {{cookiecutter.project_slug}}.{{cookiecutter.project_slug}}.main
