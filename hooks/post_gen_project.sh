9fa5a66a289b75360dde075aaa9522371bab20b4
Léopold Mebazaa <lemeb@users.noreply.github.com>
2025-09-08 23:08:35 +0200

    build(agents): make symlinks in the post gen hook

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,5 +4,9 @@ git init
 
 cp .env.example .env
 
+echo "Setting up symlinks..."
+ln -s AGENTS.md CLAUDE.md
+ln -s AGENTS.md GEMINI.md
+
 echo "✅ Template generated successfully!"
 echo '🛞 To install dependencies, run: "cd {{cookiecutter.project_slug}} && make install-all"'
